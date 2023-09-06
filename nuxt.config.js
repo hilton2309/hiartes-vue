8d74ebeef1aa2b10eab8feb7f63e67f60360496c
hilton2309 <hilton_as@hotmail.com>
2023-09-06 20:55:04 -0300

    Teste Eduardo

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/axios', '@/plugins/accessor'],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import components: https://go.nuxtjs.dev/config-components #
 
   // components: [
   //   '@/components/',
